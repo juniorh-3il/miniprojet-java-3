5b0ae478fefa808fade1ccd811865acdeabe2251
juniorh-3il <juniorh@3il.fr>
2024-03-11 11:45:43 +0100

    🚧 Wrote a basic controller

--- src/PenduControleur.java
+++ src/PenduControleur.java
@@ -1,2 +1,19 @@
-public class PenduControleur {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PenduControleur implements ActionListener {
+
+    private PenduModele model;
+    private PenduVue view;
+
+    public PenduControleur(PenduModele model, PenduVue view) {
+        this.model = model;
+        this.view = view;
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
