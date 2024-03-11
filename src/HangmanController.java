5619a4bfd3ad0fadff8f018c845876dfd966369a
juniorh-3il <juniorh@3il.fr>
2024-03-11 12:01:08 +0100

    ✨ Added buttons for each letter of the alphabet

--- src/HangmanController.java
+++ src/HangmanController.java
@@ -1,15 +1,22 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class HangmanController implements ActionListener {
 
     private HangmanModel model;
     private HangmanView view;
+    private ArrayList<JButton> letterButtons;
+    private static final char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
     public HangmanController(HangmanModel model, HangmanView view) {
         this.model = model;
         this.view = view;
-
+        this.letterButtons = new ArrayList<>();
+        for (char letter : HangmanController.ALPHABET) {
+            letterButtons.add(new JButton(String.valueOf(letter)));
+        }
     }
 
     @Override
