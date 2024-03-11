ae44aa501934915da6eca3703a12a9e1243ecbdc
juniorh-3il <juniorh@3il.fr>
2024-03-11 10:55:39 +0100

    🚧 Wrote the application entry point by reusing the one from the GUI project

--- src/PenduApplication.java
+++ src/PenduApplication.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class PenduApplication {
+
+    public static void main(String[] args) {
+        PenduModele model = new PenduModele();
+        PenduVue view = new PenduVue(model);
+        PenduControleur controler = new PenduControleur(model, view);
+
+        JFrame frame = new JFrame("Jeu du pendu");
+        frame.getContentPane().add(view);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
