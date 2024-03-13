7d054f4494dd2bb5406f11b9e5a7e4d84af528e1
juniorh-3il <juniorh@3il.fr>
2024-03-13 13:47:26 +0100

    🔥 Removed key listening in vue HangmanView.java

--- src/HangmanView.java
+++ src/HangmanView.java
@@ -3,31 +3,13 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class HangmanView extends JPanel implements KeyListener {
+public class HangmanView extends JPanel {
 
     private HangmanModel model;
 
     public HangmanView(HangmanModel model) {
         this.model = model;
         setBackground(Color.WHITE);
-        this.addKeyListener(this);
         super.setPreferredSize(new Dimension(400, 300));
-
-        // The application cannot work without those lines, but they are bad (I read it online)
-        this.setFocusable(true);
-        this.requestFocusInWindow();
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println("You pressed : " + e.getKeyChar());
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
     }
 }
